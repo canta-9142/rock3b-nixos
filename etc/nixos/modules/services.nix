ab0d952e7ee6b8a5e46b9b254ad6e2ad23c069f0
Kanta IMAI <work@floating-gate.com>
2026-08-15 14:31:37 +0900

    feat: sshd_config

--- etc/nixos/modules/services.nix
+++ etc/nixos/modules/services.nix
@@ -1,5 +1,16 @@
 { config, lib, pkgs, ... }:
 
 {
-	services.openssh.enable = true;
+	services.openssh = {
+		enable = true;
+		openFirewall = false;
+
+		settings = {
+			AllowUsers = [ "jinji" ];
+			KbdInteractiveAuthentication = false;
+			PasswordAuthentication = false;
+			PermitRootLogin = "no";
+			PubkeyAuthentication = true;
+		};
+	};
 }
