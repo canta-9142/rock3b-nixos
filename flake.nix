4ee3a805957ef8b72e8d6f6951d2cf3505113356
canta-9142 <contact@floating-gate.com>
2026-08-22 15:55:01 +0900

    feat: change directory layout

--- flake.nix
+++ flake.nix
@@ -1,58 +1,66 @@
 {
-	description = "NixOS configuration of Radxa ROCK 3B";
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
+  description = "NixOS configuration of Radxa ROCK 3B";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
 
-		pedantix.url = "github:swarsel/pedantix";
-	};
-	outputs = input@{ self,
-	                  nixpkgs,
-	                  nixpkgs-stable,
-	                  pedantix,
-	                  ... }:
-		let
-			system = "aarch64-linux";
-			devSystems = [ "aarch64-linux" "x86_64-linux" ];
-			forEachDevSystem = nixpkgs.lib.genAttrs devSystems;
-		in
-		{
-			nixosConfigurations.rock3b-nixos = nixpkgs.lib.nixosSystem {
-				inherit system;
-				specialArgs = {
-					inherit nixpkgs-stable;
-				};
+    pedantix.url = "github:swarsel/pedantix";
+  };
+  outputs =
+    input@{
+      self,
+      nixpkgs,
+      nixpkgs-stable,
+      pedantix,
+      ...
+    }:
+    let
+      system = "aarch64-linux";
+      devSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forEachDevSystem = nixpkgs.lib.genAttrs devSystems;
+    in
+    {
+      nixosConfigurations.rock3b-nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          inherit nixpkgs-stable;
+        };
 
-				modules = [
-					./configuration.nix
+        modules = [
+          ./hosts/rock3b-nixos
 
-					({ pkgs, ... }: {
-						nixpkgs.overlays = [
-							(final: prev: {
-								pkgsStable = import nixpkgs-stable {
-									inherit system;
-									config.allowUnfree = true;
-								};
-							})
-						];
-					})
-				];
-			};
+          ({ pkgs, ... }: {
+            nixpkgs.overlays = [
+              (final: prev: {
+                pkgsStable = import nixpkgs-stable {
+                  inherit system;
+                  config.allowUnfree = true;
+                };
+              })
+            ];
+          })
+        ];
+      };
 
-			devShells = forEachDevSystem (devSystem:
-				let
-					pkgs = nixpkgs.legacyPackages.${devSystem};
-				in
-				{
-					default = pkgs.mkShell {
-						packages = with pkgs; [
-							deadnix
-							git
-							nil
-							nixfmt
-							statix
-						];
-					};
-				});
-		};
+      devShells = forEachDevSystem (
+        devSystem:
+        let
+          pkgs = nixpkgs.legacyPackages.${devSystem};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              deadnix
+              git
+              nil
+              nixfmt
+              statix
+            ];
+          };
+        }
+      );
+    };
 }
---
+++ hosts/rock3b-nixos/default.nix
@@ -0,0 +1,29 @@
+{ ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/nixos/rock3b.nix
+
+    ../../modules/nixos/boot.nix
+    ../../modules/nixos/networking.nix
+    ../../modules/nixos/services.nix
+    ../../modules/nixos/packages.nix
+    ../../modules/nixos/users.nix
+
+    ../../modules/nixos/server
+  ];
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # Set your time zone.
+  time.timeZone = "Asia/Tokyo";
+
+  networking.hostName = "rock3b-nixos";
+
+  system.stateVersion = "26.05";
+
+}
